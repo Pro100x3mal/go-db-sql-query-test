c75fec728b415873b255456060706826c7cfe797
Pro100x3mal <pro100x3mal@gmail.com>
2025-04-28 22:37:13 +0300

    first iteration

--- main_test.go
+++ main_test.go
@@ -1,29 +1,62 @@
 package main
 
 import (
+	"database/sql"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	_ "modernc.org/sqlite"
 )
 
 func Test_SelectClient_WhenOk(t *testing.T) {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "./demo.db")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
 
 	clientID := 1
 
 	// напиши тест здесь
+	cl, err := selectClient(db, clientID)
+	require.NoError(t, err)
+	assert.Equal(t, clientID, cl.ID)
+	assert.NotEqual(t, "", cl.FIO)
+	assert.NotEqual(t, "", cl.Login)
+	assert.NotEqual(t, "", cl.Email)
+	assert.NotEqual(t, "", cl.Birthday)
 }
 
 func Test_SelectClient_WhenNoClient(t *testing.T) {
 	// настройте подключение к БД
-
+	db, err := sql.Open("sqlite", "./demo.db")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
 	clientID := -1
 
 	// напиши тест здесь
+	cl, err := selectClient(db, clientID)
+	require.ErrorIs(t, err, sql.ErrNoRows)
+	assert.Equal(t, 0, cl.ID)
+	assert.Equal(t, "", cl.FIO)
+	assert.Equal(t, "", cl.Login)
+	assert.Equal(t, "", cl.Email)
+	assert.Equal(t, "", cl.Birthday)
+
 }
 
 func Test_InsertClient_ThenSelectAndCheck(t *testing.T) {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "./demo.db")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
 
 	cl := Client{
 		FIO:      "Test",
@@ -33,10 +66,22 @@ func Test_InsertClient_ThenSelectAndCheck(t *testing.T) {
 	}
 
 	// напиши тест здесь
+	cl.ID, err = insertClient(db, cl)
+	require.NoError(t, err)
+	require.NotEqual(t, 0, cl.ID)
+
+	getCl, err := selectClient(db, cl.ID)
+	require.NoError(t, err)
+	assert.Equal(t, cl, getCl)
 }
 
 func Test_InsertClient_DeleteClient_ThenCheck(t *testing.T) {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "./demo.db")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
 
 	cl := Client{
 		FIO:      "Test",
@@ -46,4 +91,16 @@ func Test_InsertClient_DeleteClient_ThenCheck(t *testing.T) {
 	}
 
 	// напиши тест здесь
+	cl.ID, err = insertClient(db, cl)
+	require.NoError(t, err)
+	require.NotEqual(t, 0, cl.ID)
+
+	_, err = selectClient(db, cl.ID)
+	require.NoError(t, err)
+
+	err = deleteClient(db, cl.ID)
+	require.NoError(t, err)
+
+	_, err = selectClient(db, cl.ID)
+	require.ErrorIs(t, err, sql.ErrNoRows)
 }
